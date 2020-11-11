2c49785fc908e98167c798726b8170a5f884aa3a
Valentin <kibirevvalik202@gmail.com>
2020-11-11 11:42:14 +0300

    [update] conv

---
+++ BallFall/Assets/_Scripts/mechanism/Butter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Butter : MonoBehaviour
+{
+    [SerializeField]
+    private float _maxDistens,_mag;
+    private Vector3 _startPos;
+    void Start()
+    {
+        _startPos = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        _mag = (transform.position - _startPos).sqrMagnitude;
+        if ((transform.position - _startPos).sqrMagnitude > _maxDistens)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
