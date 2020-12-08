020f497375475102fcdc1ecb0e7b80eed6bcf853
Valentin <kibirevvalik202@gmail.com>
2020-12-08 13:52:43 +0300

    [add] Destruction

---
+++ BallFall/Assets/_Scripts/mechanism/Destruction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruction : MonoBehaviour
+{
+    [SerializeField]
+    private FixedJoint _joint;
+
+    void FixedUpdate()
+    {
+        if (_joint== null)
+        {
+            Destroy(gameObject,1);
+            this.enabled = false;
+        }
+    }
+    [ContextMenu("GetJoint")]
+    private void GetJoint()
+    {
+        _joint = GetComponent<FixedJoint>();
+    }
+}
